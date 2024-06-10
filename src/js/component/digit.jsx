05508d4e788aaef0a05e88473403303f49f75ac2
Roger Manrique <159472606+RAML84@users.noreply.github.com>
2024-06-10 20:43:27 +0000

    simplecounter

---
+++ src/js/component/digit.jsx
@@ -0,0 +1,4 @@
+//aqui creamos el import desde React
+import React from "react";
+//aqui declaramos la funcion
+export const Digit = ({number}) => <div className="digit">{number}</div>
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,54 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
+import { Digit } from "./digit.jsx";
 
 //create your first component
 const Home = () => {
+	//asincrono
+	const [timer, setTimer] = useState(0)
+	const [active, setActive] = useState(true)
+
+	useEffect(()=>{
+		if (active) {
+			setTimeout(()=>{
+				setTimer(value => value + 1)
+			}, 1000)
+		}
+	},[timer, active]) //array de dependencia
+
+		// aqui el boton maneja los valores si esta star hace stop y viceversa
+	 const startStop = () => setActive(value => !value)
+
+	 const resetTimer = () => setTimer(value => value=0)
+
 	return (
 		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<section className="counter">
+				<Digit number={<span className="fa fa-clock"></span>}/>
+				<Digit number={Math.floor(timer/100000)%10}/>
+				<Digit number={Math.floor(timer/10000)%10}/>
+				<Digit number={Math.floor(timer/1000)%10}/>
+				<Digit number={Math.floor(timer/100)%10}/>
+				<Digit number={Math.floor(timer/10)%10}/>
+				<Digit number={Math.floor(timer%10)}/>
+			</section>
+
+			<section className="container text-center">
+			<h2>counter control</h2>
+				<div>
+					<button 
+					disabled={active}
+					onClick={startStop} className="mx-1 btn btn-success">Start</button>
+					<button 
+					disabled={!active}
+					 onClick={startStop} className="mx-1 btn btn-secondary">Stop</button>
+					<button onClick={resetTimer}className="mx-1 btn btn-danger">Reset</button>
+				</div>
+
+			</section>
+			
 		</div>
 	);
 };
